35c71807dd9f693c133e40276158882d2a781230
Anatoliy <muzhik.lapotnik@gmail.com>
2021-09-20 22:15:13 +0500

    Урок 8

--- snake/Program.cs
+++ snake/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace snake
 {
@@ -6,11 +10,28 @@ namespace snake
 	{
 		static void Main(string[] args)
 		{
-			Point p1 = new Point(1, 3, '*');
-			p1.Draw();
 
-			Point p2 = new Point(4, 5, '#');
-			p2.Draw();
+
+			//Console.SetBufferSize(80, 25);
+
+			HorisontalLine line = new HorisontalLine(1,77,0,'-');
+			line.Drow();
+			HorisontalLine line1 = new HorisontalLine(0, 78, 24, '-');
+			line1.Drow();
+
+			VerticalLine line3 = new VerticalLine(1, 24, 0, '|');
+			line3.Drow();
+			VerticalLine line4 = new VerticalLine(1, 24, 78, '|');
+			line4.Drow();
+
+			Point corn1 = new Point(0, 0, '+');
+			corn1.Draw();
+			Point corn2 = new Point(78, 0, '+');
+			corn2.Draw();
+			Point corn3 = new Point(0, 24, '+');
+			corn3.Draw();
+			Point corn4 = new Point(78, 24, '+');
+			corn4.Draw();
 
 			Console.ReadLine();
 		}
---
+++ snake/VerticalLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class VerticalLine : figure
+    {
+        public VerticalLine(int xUp, int xDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = xUp; y <= xDown; y++) {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
